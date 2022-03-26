f4c0eeeb31b0857d685c1d1379fa2d91bf5e477d
Giulio <giulio.appetito.ga@gmail.com>
2022-03-26 11:23:49 +0100

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+    private String passenger;
+    private String color;
+
+    public String getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(String passenger) {
+        this.passenger = passenger;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
